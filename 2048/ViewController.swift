499a8275f31d55b07ca0945be967d2ab7d41e640
Unknown <artembobrov3059@gmail.com>
2017-09-02 22:02:01 +0200

    Initial commit

---
+++ 2048/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Artem Bobrov on 02.09.17.
+//  Copyright © 2017 Artem Bobrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
+
