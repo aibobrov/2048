bc2845b25021ac1390e4224666187dc94a4fe5b1
Unknown <artembobrov3059@gmail.com>
2017-09-08 21:39:07 +0200

    fixed bugs

--- 2048/Controllers/ViewController.swift
+++ 2048/Controllers/ViewController.swift
@@ -12,27 +12,6 @@ class ViewController: UIViewController {
 	var board: Board!
 	var controller: GameController!
 	var score: Score!
-	var resultView: Result.UIType? {
-		willSet {
-			if newValue == nil {
-				resultView?.removeFromSuperview()
-				resultView?.blurEffectView.removeFromSuperview()
-
-			}
-		}
-		didSet {
-			if let resultView = resultView {
-				resultView.value = score.value
-			}
-		}
-	}
-	var retryView: RetryView? {
-		willSet {
-			if newValue == nil {
-				retryView?.removeFromSuperview()
-			}
-		}
-	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -47,8 +26,7 @@ class ViewController: UIViewController {
 		let scorePoint = CGPoint(x: (view.frame.width + board.frame.width) / 2 - scoreSize.width, y: 30)
 		score = Score(frame: CGRect(origin: scorePoint, size: scoreSize))
 		self.view.addSubview(score)
-		
-		
+
 		setupGestures()
 		controller.restart()
 	}
@@ -63,49 +41,15 @@ extension ViewController: GameControllerDelegate {
 	}
 	
 	func userDidWon() {
-		guard resultView == nil else {
-			return
-		}
-		let rect = CGRect(origin: CGPoint.zero, size: CGSize(width: 200, height: 140))
-		resultView = Result.win(frame: rect).view
-		resultView?.center = self.view.center
-		self.view.addSubview(resultView!)
-		self.resultView?.setBlurView()
-		self.resultView?.blurEffectView.alpha = 0
-		UIView.animate(withDuration: 0.3, animations: {
-			self.resultView?.blurEffectView.alpha = 1.0
-		}, completion: {_ in
-			guard self.retryView == nil, let resultView = self.resultView else {
-				return
-			}
-			let finalPoint = CGPoint(x: resultView.frame.minX, y: resultView.frame.maxY + 16)
-			
-			self.retryView = RetryView(frame: CGRect(origin: CGPoint(x: finalPoint.x, y: self.view.frame.maxY), size: CGSize(width: resultView.frame.width, height: resultView.frame.height / 2)) , clicked: #selector(self.retryClicked))
-			self.view.addSubview(self.retryView!)
-			UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 5, initialSpringVelocity: 5, options: .curveEaseOut, animations:{
-				self.retryView?.frame.origin.y = finalPoint.y
-			}, completion: nil)
-		})
-		
 		print("win")
 	}
 	
 	@objc func retryClicked() {
-		self.controller.restart()
-		retryView = nil
-		resultView = nil
-		print("retry")
+
 	}
+
 	func userDidLost() {
-		guard resultView == nil else {
-			return
-		}
-		let rect = CGRect(origin: CGPoint.zero, size: CGSize(width: 200, height: 140))
-		resultView = Result.lose(frame: rect).view
-		resultView?.center = self.view.center
-		self.view.addSubview(resultView!)
-		resultView?.setBlurView()
-		print("ended")
+
 	}
 	
 	func scoreDidChanged(to score: Int) {
